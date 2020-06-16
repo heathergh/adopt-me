34f82097da93eb32d77c4d2470c4ed70c298b943
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-06-16 19:34:45 -0400

    Added animals module from frontend masters; created custom hook to generate dropdowns

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { animals } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", animals);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input type="text" id="location" value={location} />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
+
+        <AnimalDropdown />
+        <BreedDropdown />
         <button type="submit">Submit</button>
       </form>
     </div>
